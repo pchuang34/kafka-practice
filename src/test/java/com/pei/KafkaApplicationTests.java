18f5c841d67d3ce79eb73a2c33368ecba1dd2024
Pei Cheng Huang <peicheng@vt.edu>
2023-04-19 16:04:04 -0700

    kafka topic, producer config, template send

--- src/test/java/com/pei/KafkaApplicationTests.java
+++ src/test/java/com/pei/KafkaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.kafkapractice;
+package com.pei;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkaPracticeApplicationTests {
+class KafkaApplicationTests {
 
     @Test
     void contextLoads() {
